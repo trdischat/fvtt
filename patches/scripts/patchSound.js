83f37f5e6d0d2d1b3e42677785772a74ee35ce97
Taylor Reid <trgit@hoghill.net>
2019-07-30 17:54:25 -0700

    add random delay between sounds

--- patches/scripts/patchSound.js
+++ patches/scripts/patchSound.js
@@ -29,3 +29,48 @@ if (patchedPlaylistDirectoryClass == undefined) {
   patchPlaylistDirectoryClass();
   var patchedPlaylistDirectoryClass = true;
 }
+
+/**
+ * Patch Playlist class to implement random loop delays
+ */
+function patchPlaylistClass() {
+    newClass = patchClass(Playlist, Playlist.prototype._onEnd, 5,
+      `if ( sound.repeat ) return;`,
+      `if ( sound.repeat ) {
+      if ( sound.flags.mindelay === undefined || sound.flags.maxdelay === undefined) return;
+      else {
+        let h = this.howls[sound.id].howl;
+        h.stop(howlId);
+        let tMin = sound.flags.mindelay;
+        let tMax = sound.flags.maxdelay;
+        let tDelay = Math.floor( 1000 * ( tMin + ( Math.random() * (tMax-tMin) ) ) );
+        setTimeout( function() { h.play(howlId); }, tDelay);
+        return;}}`);
+    if (!newClass) return;
+    Playlist = newClass
+}
+
+if (patchedPlaylistClass == undefined) {
+  patchPlaylistClass();
+  var patchedPlaylistClass = true;
+}
+
+/**
+ * Use custom template for PlaylistSoundConfig to set range of pause between sounds 
+ */
+replaceDefaultOptions = function(class_) {
+  defaultOptionsProperty = Object.getOwnPropertyDescriptor(class_, "defaultOptions");
+  if (defaultOptionsProperty == undefined) {
+    defaultOptionsProperty = Object.getOwnPropertyDescriptor(FormApplication, "defaultOptions");
+  }
+  Object.defineProperty(class_, '_sound_config_defaultOptions', defaultOptionsProperty);
+  Object.defineProperty(class_, 'defaultOptions', {
+    get: function () {
+      def_options = class_._sound_config_defaultOptions;
+      def_options.template = def_options.template.replace("templates/playlist/", "public/modules/patches/templates/")
+      return def_options
+    }
+  });
+};
+
+replaceDefaultOptions(PlaylistSoundConfig);
