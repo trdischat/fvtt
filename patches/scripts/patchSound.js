ba7010c47c41c0defacc428e507093f82506ca6e
trdischat <trdischat@hoghill.net>
2019-11-09 11:11:59 -0800

    update modules for FVTT 0.3.9

--- patches/scripts/patchSound.js
+++ patches/scripts/patchSound.js
@@ -1,20 +1,4 @@
 /**
- * Patch SoundsLayer class to apply volume easing to selected volume level, not max volume level
- */
-function patchSoundsLayerClass() {
-    newClass = patchClass(SoundsLayer, SoundsLayer.prototype.update, 34,
-      `if ( obj[p].volume === 0 || obj[p].volume < vol ) obj[p].volume = vol;`,
-      `if ( obj[p].volume === 0 || obj[p].volume < vol ) obj[p].volume = vol * sound.data.volume;`);
-    if (!newClass) return;
-    SoundsLayer = newClass
-}
-
-if (patchedSoundsLayerClass == undefined) {
-  patchSoundsLayerClass();
-  var patchedSoundsLayerClass = true;
-}
-
-/**
  * Patch PlaylistDirectory class to display playlists only to the GM
  */
 function patchPlaylistDirectoryClass() {
